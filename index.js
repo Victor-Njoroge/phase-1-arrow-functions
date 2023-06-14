136549e90a3f59604f24a932d430f9425cc27268
Victor <victornjoroge2020@gmail.com>
2023-06-14 12:54:04 +0300

    Done Assignment

---
+++ index.js
@@ -0,0 +1,19 @@
+const divide = function(num1=2000, num2=100, divides){
+    divides = num1/num2
+    return divides;
+}
+divide();
+
+
+const square = (num4=5, squared)=>{
+     squared = num4*num4
+    return squared;
+}
+square();
+
+
+const add = (num5=56, num6=46, addition) =>{
+    addition=num5+num6;
+    return addition;
+}
+add();
